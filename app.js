c049d9f292a1ab8f77073e56aaeae4810d723230
thelizabethz <natthawut.not31@gmail.com>
2024-05-28 15:19:05 +0700

    add products.json

--- app.js
+++ app.js
@@ -5,29 +5,33 @@ const debug = createDebug('app')
 import morgan from 'morgan';
 import path from 'path';
 const app = express();
+const products = require("./data/products.json");
 const PORT = process.env.PORT
 const __dirname = path.resolve();
+
 const productRouter = express.Router();
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, "/public/")))
 
-app.set("views","./src/views");
-app.set("view engine","ejs");
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
 
-productRouter.route("/").get((req,res) =>{
-    res.send("Hello I'm Product");
+productRouter.route("/").get((req, res) => {
+    res.render("products", 
+        products,
+    );
 });
 
-productRouter.route("/1").get((req,res) =>{
+productRouter.route("/1").get((req, res) => {
     res.send("Hello I'm Product 1");
 });
 
-app.use("/products",productRouter)
+app.use("/products", productRouter)
 
 app.get("/", (req, res) => {
 
-    res.render('index',{username:'natthawut'});
+    res.render('index', { username: 'natthawut' });
 
 })
 
