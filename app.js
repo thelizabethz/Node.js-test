b13ca3ec8735bef9d507b57d40b137adc9ecee14
thelizabethz <natthawut.not31@gmail.com>
2024-06-14 09:20:19 +0700

    create router

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-import express from 'express';
 import chalk from 'chalk';
 import createDebug from 'debug'
 const debug = createDebug('app')
 import morgan from 'morgan';
 import path from 'path';
-import fs from "fs";
-const app = express();
-const products = JSON.parse(fs.readFileSync("./data/products.json"));
+
 const PORT = process.env.PORT
 const __dirname = path.resolve();
+import fs from "fs";
+const productsRouter = fs.readFileSync("./src/router/productsRouter");
 
-const productRouter = express.Router();
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, "/public/")))
@@ -18,21 +16,9 @@ app.use(express.static(path.join(__dirname, "/public/")))
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
-productRouter.route("/").get((req, res) => {
-    res.render("products",{ 
-        products,
-    }
-    );
-});
 
-productRouter.route("/:id").get((req, res) => {
-    const id = req.params.id;
-    res.render("product",{
-        product: products[id],
-    })
-});
 
-app.use("/products", productRouter)
+app.use("/products", productsRouter)
 
 app.get("/", (req, res) => {
 
